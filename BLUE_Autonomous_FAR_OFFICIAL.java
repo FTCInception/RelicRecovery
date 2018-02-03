24252ffe44ac9c36306f8306ef2aac6f8e2de94e
FTCInception <34779739+FTCInception@users.noreply.github.com>
2018-02-02 21:34:23 -0600

    Add files via upload

--- BLUE_Autonomous_FAR_OFFICIAL.java
+++ BLUE_Autonomous_FAR_OFFICIAL.java
@@ -12,28 +12,29 @@ package org.firstinspires.ftc.teamcode.nolan;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 
 //@com.qualcomm.robotcore.eventloop.  opmode.TeleOp(name="Nolan v3", group="TeleOp")
 @Autonomous(name="Blue_Position_FAR", group="BLUE_FAR")
 
 public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
 
-    public static final String TAG = "Vuforia VuMark Sample";
+    public static final String TAG = "Vuforia VuMark";
     private static DcMotor l_f_motor, l_b_motor, r_f_motor, r_b_motor, relic_motor;
     private static Servo gripperFlipper, relicGripper, relicTooth;
-    private static CRServo  l_t_gripper, r_t_gripper, l_b_gripper, r_b_gripper;
+    private static Servo  l_t_gripper, r_t_gripper, l_b_gripper, r_b_gripper;
     private static DcMotor lifter_motor;
     private static Servo jewel_hand, jewel_elbow;
     private final int TICS_PER_REV = 1120;
@@ -41,16 +42,23 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
     private final double wheelRatio = (25/16);
     private static boolean open_gripper;
 
+    OpenGLMatrix lastLocation = null;
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    VuforiaLocalizer vuforia;
 
     public void initialize() {
         l_f_motor = hardwareMap.dcMotor.get("left_front");
-        l_f_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        l_f_motor.setDirection(DcMotorSimple.Direction.REVERSE);
         l_b_motor = hardwareMap.dcMotor.get("left_back");
-        l_b_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        l_b_motor.setDirection(DcMotorSimple.Direction.REVERSE);
         r_f_motor = hardwareMap.dcMotor.get("right_front");
-        r_f_motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        r_f_motor.setDirection(DcMotorSimple.Direction.FORWARD);
         r_b_motor = hardwareMap.dcMotor.get("right_back");
-        r_b_motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        r_b_motor.setDirection(DcMotorSimple.Direction.FORWARD);
         //NOTE In the example the DcMotorController.RunMode was used, but this wasn't working so I changed it to DcMotor.RunMode
         relic_motor = hardwareMap.dcMotor.get("relic_motor");
         relic_motor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -61,10 +69,10 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
         lifter_motor.setDirection(DcMotorSimple.Direction.REVERSE);
 
    /* GRIPPERS: */
-        l_b_gripper = hardwareMap.crservo.get("left_bottom_arm");
-        r_b_gripper = hardwareMap.crservo.get("right_bottom_arm");
-        l_t_gripper = hardwareMap.crservo.get("left_top_arm");
-        r_t_gripper = hardwareMap.crservo.get("right_top_arm");
+        l_b_gripper = hardwareMap.servo.get("left_bottom_arm");
+        r_b_gripper = hardwareMap.servo.get("right_bottom_arm");
+        l_t_gripper = hardwareMap.servo.get("left_top_arm");
+        r_t_gripper = hardwareMap.servo.get("right_top_arm");
         gripperFlipper = hardwareMap.servo.get("gripper_flipper");
 
         relicGripper = hardwareMap.servo.get("relic_arm");
@@ -77,8 +85,8 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
         open_gripper = true;
 
         //not sure about these positions
-        jewel_elbow.setPosition(0.4);
-        jewel_hand.setPosition(0.5);
+        jewel_elbow.setPosition(.6);
+        jewel_hand.setPosition(0.4);
 
     }
 
@@ -158,7 +166,7 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
     public void turn(boolean ccw)
     {
         ElapsedTime eTime = new ElapsedTime();
-        if (ccw){
+         if (ccw){
             while (eTime.time()<6 && opModeIsActive()){
                 r_f_motor.setPower(-.2);
                 r_b_motor.setPower(-.2);
@@ -244,15 +252,15 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
 
     }
     //These methods handle moving to each cryptobox column during autonomous, as determined by the result of the vumark key
-    public void right()
+    //public void right()
     {
         strafe(true,.4);
         movement(getDistance(5.0),-.25);
-        l_b_gripper.setPower(-.2);
-        r_b_gripper.setPower(-.2);
+        //l_b_gripper.setPower(-.2);
+        //r_b_gripper.setPower(-.2);
         sleep(2000);
-        l_b_gripper.setPower(0);
-        r_b_gripper.setPower(0);
+        //l_b_gripper.setPower(0);
+        //r_b_gripper.setPower(0);
         strafe(true,.5);
 
     }
@@ -260,83 +268,102 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
     {
         strafe(true,4);
         movement(getDistance(5.0),-.25);
-        l_b_gripper.setPower(-.2);
-        r_b_gripper.setPower(-.2);
+        //l_b_gripper.setPower(-.2);
+      //  r_b_gripper.setPower(-.2);
         sleep(2000);
-        l_b_gripper.setPower(0);
-        r_b_gripper.setPower(0);
+     //   l_b_gripper.setPower(0);
+     //  r_b_gripper.setPower(0);
         strafe(false,.5);
     }
     public void center()
     {
         strafe(true,2);
         movement(getDistance(5.0),-.25);
-        l_b_gripper.setPower(-.2);
-        r_b_gripper.setPower(-.2);
+      //  l_b_gripper.setPower(-.2);
+     //  r_b_gripper.setPower(-.2);
         sleep(2000);
-        l_b_gripper.setPower(0);
-        r_b_gripper.setPower(0);
+     //   l_b_gripper.setPower(0);
+     //   r_b_gripper.setPower(0);
     }
 
-    OpenGLMatrix lastLocation = null;
-    VuforiaLocalizer vuforia;
     @Override
     public void runOpMode() {
         initialize();
         //Get Vuforia stuff ready
+        /*
+         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
+         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
+         */
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        // OR...  Do Not Activate the Camera Monitor View, to save power
+        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
         parameters.vuforiaLicenseKey = "AUOhDYf/////AAAAmdDp0b8XRUxwrHihX1jNl15KBIqZaXwn3EnrN6lJDn3BlYAco4a9MdghQnfu+QX+MUaAV3x8eOywibefAugDVdRRLjjYJGSNbljXvztRpkFYIrLZPfKGwHKibcGilsdTGiQebl4+lnGWCY4CzVx7lVFuXY6qWQ5lFAOGaJEUOHvQ1/R8iWUOyOg9RRXalnDsSsvvIoGN3cvqxq4xfcIb2r/Az+UQQTsi73p+GVzrYMDegDQ422tXTcSaLO5Kp6MA6+OfyuuF19nSsTd+5L6Zn3se9oVdsq+fcmffIzoVHaCVT+3rEVqQBngW+viXAvcHMATyVyZB2ZOG3sgBCQDpjPYZ0sKClH56Zj3x9RPnHFeI";
+
+        /*
+         * We also indicate which camera on the RC that we wish to use.
+         * Here we chose the back (HiRes) camera (for greater range), but
+         * for a competition robot, the front camera might be more convenient.
+         */
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        /**
+         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
+         * in this data set: all three of the VuMarks in the game were created from this one template,
+         * but differ in their instance id information.
+         * @see VuMarkInstanceId
+         */
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate");
+        VuforiaTrackable relicBoy = relicTrackables.get(0);
+
 
 
         ColorSensor sensorColor;
         sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color");
+
         waitForStart();
+
         relicTrackables.activate();
         //start by closing the gripper on the block
 
 
 
         //rotate the Linear Actuator to be parallel to the ground
-        jewel_hand.setPosition(.55);
-        jewel_elbow.setPosition(1.0);
-
-        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-        sleep(3000);
-
+        jewel_hand.setPosition(1);
+        jewel_elbow.setPosition(.3575);
 
 
 
         if (sensorColor.red() < sensorColor.blue()) {
+
             //the color of the ball facing the sensor is red
             //we need to turn our arm to hit it off
             sleep(500);
-            jewel_hand.setPosition(.4);
+            jewel_elbow.setPosition(.1);
             sleep(100);
-            jewel_hand.setPosition(.5);
+            jewel_elbow.setPosition(.5);
             sleep(100);
-            jewel_hand.setPosition(.4);
+            jewel_elbow.setPosition(.1);
             sleep(100);
-            jewel_hand.setPosition(.5);
+            jewel_elbow.setPosition(.5);
             telemetry.addData("Color found is", "red");
 
 
         } else if (sensorColor.blue() < sensorColor.red()) {
+
             //the color of the ball facing the sensor is blue
             //we need to turn our arm to hit it off
             sleep(500);
-            jewel_hand.setPosition(1);
+            jewel_elbow.setPosition(1);
             sleep(100);
-            jewel_hand.setPosition(.5);
+            jewel_elbow.setPosition(.5);
             sleep(100);
-            jewel_hand.setPosition(1);
+            jewel_elbow.setPosition(1);
             sleep(100);
-            jewel_hand.setPosition(.5);
+            jewel_elbow.setPosition(.5);
             telemetry.addData("Color found is", "blue");
 
 
@@ -345,43 +372,53 @@ public class BLUE_Autonomous_FAR_OFFICIAL extends LinearOpMode{
             telemetry.addData("Color found is", "none lol git gud scrub");
             sleep(2000);
         }
+        telemetry.update();
         jewel_elbow.setPosition(.4);
         jewel_hand.setPosition(.5);
+
         boolean found = false;
-        telemetry.addData("VuMark", "%s visible", vuMark);
-            sleep(5000);
-            if (vuMark == RelicRecoveryVuMark.CENTER)
-            {
+        ElapsedTime etime = new ElapsedTime();
+        while (!found && etime.time()<15) {
+
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicBoy);
+
+            telemetry.addData("VuMark", "%s visible", vuMark);
+
+            if (vuMark == RelicRecoveryVuMark.CENTER) {
                 telemetry.addData("VuMark", "%s visible", vuMark);
                 getOff();
                 center();
                 found = true;
             }
-            if (vuMark == RelicRecoveryVuMark.RIGHT)
-            {
+            if (vuMark == RelicRecoveryVuMark.RIGHT) {
                 telemetry.addData("VuMark", "%s visible", vuMark);
                 getOff();
-                right();
+                //right();
                 found = true;
             }
-            if (vuMark == RelicRecoveryVuMark.LEFT)
-            {
+            if (vuMark == RelicRecoveryVuMark.LEFT) {
                 telemetry.addData("VuMark", "%s visible", vuMark);
                 getOff();
                 left();
                 found = true;
             }
-        //movement(getDistance(15),.15);
-        //turn(true);
 
 
 
-        if(found == false) {
-            telemetry.addData("VuMark", "not visible");
-            //prayer(eTime);
+            if (found == false) {
+                telemetry.addData("VuMark", "not visible");
+                //prayer(eTime);
+            }
+            telemetry.update();
+        }
+        //WE COULDN'T READ THE VUMARK AND SUFFER
+        if (!found)
+        {
+            telemetry.addData("We didn't find it...", "Hold this L");
+            getOff();
+            center();
         }
         telemetry.update();
-
 
 
 
